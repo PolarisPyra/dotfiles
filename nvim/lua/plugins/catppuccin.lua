f721b176f08a7efe8ea4a99ec89ede44c84a6d76
Polaris <tmillerklugman@gmail.com>
2023-03-06 16:21:46 -0500

    added nvim config

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+-- add gruvbox
+return {
+  { "catppuccin/nvim", name = "catppuccin" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
