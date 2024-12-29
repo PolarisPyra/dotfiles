bd022e9d265b5bf663109e20e47beeacdf6630f9
Polaris <cozynet503@gmail.com>
2024-12-29 13:27:18 -0500

    update nvim keybinds

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -38,10 +38,10 @@ vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
 -- Resize with arrows
-vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+--vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
+--vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
+--vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
+--vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
 
 -- Buffers
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
