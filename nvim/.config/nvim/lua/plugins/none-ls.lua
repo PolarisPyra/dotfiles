ead9f7f996ddcac14d9bcf3fc61acfae16da4865
Taylor Miller-Klugman <tmillerklugman@gmail.com>
2025-02-28 00:10:06 -0500

    fixed eslint

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,4 +1,3 @@
--- Format on save and linters
 return {
     'nvimtools/none-ls.nvim',
     dependencies = {
@@ -6,10 +5,10 @@ return {
         'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
     },
     config = function()
-        local null_ls = require 'null-ls'
+        local null_ls = require('null-ls')
         local formatting = null_ls.builtins.formatting -- to setup formatters
         local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
+        
         -- list of formatters & linters for mason to install
         require('mason-null-ls').setup {
             ensure_installed = {
@@ -23,25 +22,26 @@ return {
             -- auto-install configured formatters & linters (with null-ls)
             automatic_installation = true,
         }
-
+        
         local sources = {
-            diagnostics.eslint_d,   
             diagnostics.checkmake,
             formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown', 'typescript', 'typescriptreact', 'js', 'jsx' } },
             formatting.stylua,
             formatting.shfmt.with { args = { '-i', '4' } },
             formatting.terraform_fmt,
+            -- Use the none-ls-extras version of eslint_d
+            require('none-ls.diagnostics.eslint_d'),
             require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } },
-            require 'none-ls.formatting.ruff_format',
+            require('none-ls.formatting.ruff_format'),
         }
-
+        
         local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
         null_ls.setup {
             -- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
             sources = sources,
             -- you can reuse a shared lspconfig on_attach callback here
             on_attach = function(client, bufnr)
-                if client.supports_method 'textDocument/formatting' then
+                if client.supports_method('textDocument/formatting') then
                     vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
                     vim.api.nvim_create_autocmd('BufWritePre', {
                         group = augroup,
