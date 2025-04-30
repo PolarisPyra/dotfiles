992b5f4755f9b8db14c84dd4f2888723f6966a1e
PolarisPyra <tmillerklugman@gmail.com>
2025-04-29 21:13:53 -0400

    format lua

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,4 +1,6 @@
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {
+    desc = "Open Parent Directory in Oil"
+})
 
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,29 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function () 
-			local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-					ensure_installed = {
-						"tsx",
-						"toml",
-						"json",
-						"css",
-						"html",
-						"lua"
-					},
-					auto_install = true,
-					sync_install = false,
-					highlight = { enable = true },
-					indent = { enable = true },  
+        configs.setup({
+            ensure_installed = {"tsx", "toml", "json", "css", "html", "lua"},
+            auto_install = true,
+            sync_install = false,
+            highlight = {
+                enable = true
+            },
+            indent = {
+                enable = true
+            },
 
-					incremental_selection = {
-							enable = true,
-							keymaps = {
-									init_selection = "<Enter>",
-									node_incremental = "<Enter>",
-									scope_incremental = false,
-									node_decremental = "<Backspace>",
-							},
-					},
-			})
-	end
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<Enter>",
+                    node_incremental = "<Enter>",
+                    scope_incremental = false,
+                    node_decremental = "<Backspace>"
+                }
+            }
+        })
+    end
 }
