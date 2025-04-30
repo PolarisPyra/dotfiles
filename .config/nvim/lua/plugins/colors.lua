cf75da1b6988a3ac4962957454459d71931c4705
PolarisPyra <tmillerklugman@gmail.com>
2025-04-29 20:43:12 -0400

    added statusline

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,24 +1,21 @@
-local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-end
-
-return { 
-    {
-	"scottmckendry/cyberdream.nvim",
-	lazy = false, 
-	priority = 1000,
-	config = function() 
-	    vim.cmd.colorscheme "cyberdream"
-	    enable_transparency()
-	end
-    },
-    {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-	    theme = 'cyberdream'
-	},
-    },
+return {
+    "rebelot/kanagawa.nvim",
+    branch="master",
+    config=function()
+        require('kanagawa').setup({
+           -- transparent=true,
+            overrides=function(colors)
+                return {
+                    ["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)
+                    ["@markup.link.label.markdown_inline"] = { link = "WarningMsg" }, -- [label]
+                    ["@markup.italic.markdown_inline"] = { link = "Exception" }, -- *italic*
+                    ["@markup.raw.markdown_inline"] = { link = "String" }, -- `code`
+                    ["@markup.list.markdown"] = { link = "Function" }, -- + list
+                    ["@markup.quote.markdown"] = { link = "Error" }, -- > blockcode
+                    ["@markup.list.checked.markdown"] = { link = "WarningMsg" } -- - [X] checked list item
+                }
+            end
+        });
+        vim.cmd("colorscheme kanagawa");
+    end,
 }
