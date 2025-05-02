51ff70b603c6e232f8f2608eb34a9d05b5181d3a
PolarisPyra <tmillerklugman@gmail.com>
2025-05-02 13:06:06 -0400

    rosepine

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -2,6 +2,21 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
+		require("rose-pine").setup({
+			variant = "main",
+			dark_variant = "main",
+			dim_inactive_windows = false,
+			extend_background_behind_borders = true,
+
+			enable = {
+				terminal = true,
+				legacy_highlights = true,
+			},
+			styles = {
+				transparency = true,
+			},
+		})
+
 		vim.cmd("colorscheme rose-pine")
 	end,
 }
