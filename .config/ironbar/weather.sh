5233c93393161050d986c1542cfae055e0f4226a
PolarisPyra <tmillerklugman@gmail.com>
2025-05-08 22:15:34 -0400

    udev backup

--- .config/ironbar/weather.sh
+++ .config/ironbar/weather.sh
@@ -1,1 +1,1 @@
-ansiweather -l Plymouth,US -u imperial | sed -r "s/\x1B\[[0-9;]*[mGK]//g" | awk '{print "Plymouth, US:", $4}'
+ansiweather -l Plymouth,US -u imperial -a false | sed -E 's/^([^-]*: [0-9]+ °F).*/\1/'
