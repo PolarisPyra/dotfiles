dfdccc54a9285762bfd8fa89b3da114ceb5ed5cf
PolarisPyra <tmillerklugman@gmail.com>
2025-05-30 16:57:31 -0400

    tmux multiplex

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,27 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- multiplexer
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 12
+
+config.enable_tab_bar = false
+
+config.color_scheme = "rose-pine-moon"
+
+-- Override color scheme settings bc highlight fucking sucks for some reason
+config.colors = {
+	selection_bg = "#44415a",
+}
+
+config.animation_fps = 1
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_rate = 800
+
+-- tmux
+-- Leader is the same as my old tmux prefix
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- splitting
@@ -36,26 +56,5 @@ config.keys = {
 		}),
 	},
 }
-
--- style type shit frfr
-config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 12
-
-config.enable_tab_bar = false
-
-config.color_scheme = "rose-pine-moon"
-
--- Override color scheme settings bc highlight fucking sucks for some reason
-config.colors = {
-	selection_bg = "#44415a",
-}
-
--- blinking cursor fps
-config.animation_fps = 1
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
-
-config.default_cursor_style = "BlinkingBar"
-config.cursor_blink_rate = 800
 
 return config
