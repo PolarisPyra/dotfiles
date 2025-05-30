3bf162b21ef1e52ceda60e5d840f783b563d25f0
PolarisPyra <tmillerklugman@gmail.com>
2025-05-30 17:19:30 -0400

    its alllivveee

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,51 +1,5 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
-
-local function is_vim(pane)
-	-- this is set by the plugin, and unset on ExitPre in Neovim
-	return pane:get_user_vars().IS_NVIM == "true"
-end
-
-local direction_keys = {
-	Left = "h",
-	Down = "j",
-	Up = "k",
-	Right = "l",
-	-- reverse lookup
-	h = "Left",
-	j = "Down",
-	k = "Up",
-	l = "Right",
-}
-
-local function split_nav(resize_or_move, key)
-	return {
-		key = key,
-		mods = resize_or_move == "resize" and "META" or "CTRL",
-		action = wezterm.action_callback(function(win, pane)
-			if is_vim(pane) then
-				-- pass the keys through to vim/nvim
-				win:perform_action({
-					SendKey = {
-						key = key,
-						mods = resize_or_move == "resize" and "META" or "CTRL",
-					},
-				}, pane)
-			else
-				if resize_or_move == "resize" then
-					win:perform_action({
-						AdjustPaneSize = { direction_keys[key], 3 },
-					}, pane)
-				else
-					win:perform_action({
-						ActivatePaneDirection = direction_keys[key],
-					}, pane)
-				end
-			end
-		end),
-	}
-end
 
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 12
@@ -69,8 +23,6 @@ config.cursor_blink_rate = 800
 -- tmux
 -- Leader is the same as my old tmux prefix
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
-
--- Combine all keybindings
 config.keys = {
 	-- splitting
 	{
@@ -88,30 +40,16 @@ config.keys = {
 		key = "m",
 		action = wezterm.action.TogglePaneZoomState,
 	},
-	-- rotate panes
-	{
-		mods = "LEADER",
-		key = "Space",
-		action = wezterm.action.RotatePanes("Clockwise"),
-	},
-	-- show the pane selection mode, but have it swap the active and selected panes
-	{
-		mods = "LEADER",
-		key = "0",
-		action = wezterm.action.PaneSelect({
-			mode = "SwapWithActive",
-		}),
-	},
-	-- move between split panes
-	split_nav("move", "h"),
-	split_nav("move", "j"),
-	split_nav("move", "k"),
-	split_nav("move", "l"),
-	-- resize panes
-	split_nav("resize", "h"),
-	split_nav("resize", "j"),
-	split_nav("resize", "k"),
-	split_nav("resize", "l"),
 }
+
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+
+smart_splits.apply_to_config(config, {
+	direction_keys = { "h", "j", "k", "l" },
+	modifiers = {
+		move = "CTRL",
+		resize = "META",
+	},
+})
 
 return config
