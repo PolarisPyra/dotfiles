ebc020faa960fc0b21d99dd09d12ba92c968b554
PolarisPyra <tmillerklugman@gmail.com>
2025-05-29 20:29:16 -0400

    why

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,6 +9,11 @@ config.enable_tab_bar = false
 
 config.color_scheme = "rose-pine-moon"
 
+-- Override color scheme settings bc highlight fucking sucks for some reason
+config.colors = {
+    selection_bg = "#44415a"
+}
+
 config.animation_fps = 1
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
